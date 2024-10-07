f76c1b731a258df5f74a4721f95319e6bb682f12
tiyu25 <tiyuuuu9@email.com>
2024-10-07 12:17:26 +0900

    [Level 1] JWT의 이해

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -16,6 +16,7 @@ public class User extends Timestamped {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
+    private String nickname;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
